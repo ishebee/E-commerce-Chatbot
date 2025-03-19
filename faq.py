86a0b0db29249466968b5cba361f4b4e88d8b1ec
Shebeeb S <shebeebshe@gmail.com>
2025-03-19 20:42:22 +0530

    Update faq.py

--- faq.py
+++ faq.py
@@ -3,21 +3,23 @@ from pathlib import Path
 import chromadb
 from chromadb.utils import embedding_functions
 from groq import Groq
-import pandas
+import pandas as pd
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
+# Load API Key from Streamlit Secrets
+GROQ_MODEL = st.secrets["GROQ_MODEL"]
 
 ef = embedding_functions.SentenceTransformerEmbeddingFunction(
     model_name='sentence-transformers/all-MiniLM-L6-v2'
 )
 faqs_path = Path(__file__).parent / "resources/faq_data.csv"
 chroma_client = chromadb.Client()
-groq_client = Groq()
+groq_client = Groq(api_key=GROQ_MODEL)
 collection_name_faq = 'faqs'
-
 
 def ingest_faq_data(path):
     if collection_name_faq not in [c.name for c in chroma_client.list_collections()]:
@@ -26,7 +28,7 @@ def ingest_faq_data(path):
             name=collection_name_faq,
             embedding_function=ef
         )
-        df = pandas.read_csv(path)
+        df = pd.read_csv(path)
         docs = df['question'].to_list()
         metadata = [{'answer': ans} for ans in df['answer'].to_list()]
         ids = [f"id_{i}" for i in range(len(docs))]
@@ -37,20 +39,7 @@ def ingest_faq_data(path):
         )
         print(f"FAQ Data successfully ingested into Chroma collection: {collection_name_faq}")
     else:
-        print(f"Collection: {collection_name_faq} already exist")
-
-
-def get_relevant_qa(query):
-    collection = chroma_client.get_collection(
-        name=collection_name_faq,
-        embedding_function=ef
-    )
-    result = collection.query(
-        query_texts=[query],
-        n_results=2
-    )
-    return result
-
+        print(f"Collection: {collection_name_faq} already exists")
 
 def generate_answer(query, context):
     prompt = f'''Given the following context and question, generate answer based on this context only.
@@ -61,29 +50,9 @@ def generate_answer(query, context):
     QUESTION: {query}
     '''
     completion = groq_client.chat.completions.create(
-        model=os.environ['GROQ_MODEL'],
+        model=GROQ_MODEL,
         messages=[
-            {
-                'role': 'user',
-                'content': prompt
-            }
+            {'role': 'user', 'content': prompt}
         ]
     )
     return completion.choices[0].message.content
-
-
-def faq_chain(query):
-    result = get_relevant_qa(query)
-    context = "".join([r.get('answer') for r in result['metadatas'][0]])
-    #print("Context:", context)
-    answer = generate_answer(query, context)
-    return answer
-
-
-if __name__ == '__main__':
-    ingest_faq_data(faqs_path)
-    query = "what's your policy on defective products?"
-    query = "Do you take cash as a payment option?"
-    # result = get_relevant_qa(query)
-    answer = faq_chain(query)
-    print("Answer:", answer)
